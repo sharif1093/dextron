83bdd61b75640bf7364da487e7b54152ffd536e5
mrsharif <sharif1093@gmail.com>
2019-08-25 14:44:28 -0400

    Move sync to the main runner where it's needed.

--- dextron/pipeline/playback_runner.py
+++ dextron/pipeline/playback_runner.py
@@ -97,7 +97,8 @@ class PlaybackRunner (Runner):
                     self.state["i_cycle"] += 1
                 # End of Cycle
 
-                self._inc_epoch()
+                self.state["i_epoch"] += 1
+                self.monitor_epoch()
                 # NOTE: We may save/test after each cycle or at intervals.
 
                 # 1. Perform the test
@@ -113,18 +114,6 @@ class PlaybackRunner (Runner):
         finally:
             logger.fatal('End of operation ...')
             self.finalize()
-    
-    def _sync_normalizations(self, main_explorer, target_explorer):
-        state_dict = self.explorer[main_explorer].state_dict()
-        
-        keys = ["digideep.environment.wrappers.normalizers:VecNormalizeObsDict", "digideep.environment.wrappers.normalizers:VecNormalizeRew"]
-        
-        state_dict_mod = {}
-        for k in keys:
-            if k in state_dict["envs"]:
-                state_dict_mod[k] = state_dict["envs"][k]
-
-        self.explorer[target_explorer].envs.load_state_dict(state_dict_mod)
 
 
 # env.physics.get_state()
